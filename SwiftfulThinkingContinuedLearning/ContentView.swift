20e56ce7762981249dadc84facbd5239ebe44398
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-05-17 12:27:18 -0500

    Initial Commit

---
+++ SwiftfulThinkingContinuedLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Griffin Baker on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinuedLearningApp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Griffin Baker on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
